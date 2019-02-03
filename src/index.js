2154a400dd07c8057a9406ab9505f168fe898bef
michaelknoch <info@michaelknoch.de>
2019-02-03 13:47:37 +0100

    init

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const title = "My Minimal React sachen Webpack Babel Setup";
+
+ReactDOM.render(
+    <div style={{ backgroundColor: "blue" }}>{title}</div>,
+    document.getElementById("app")
+);
